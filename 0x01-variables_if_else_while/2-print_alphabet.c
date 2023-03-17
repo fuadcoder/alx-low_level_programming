35195bbb0347e9807aa02988378a2740037947ff
Horlams <olasupoolamide00@gmail.com>
2023-03-17 16:00:49 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Prints the alphabet in lowercase
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
 
+	for (i = 0; i < 26; i++)
+	{
+		putchar(alp[i]);
+	}
+	putchar("\n");
+	return (0);
+}

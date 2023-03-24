30ed1868816b7106f1e11402ce5b273aaafc0761
Horlams <olasupoolamide00@gmail.com>
2023-03-24 14:07:25 -0700

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,15 +2,16 @@
 
 /**
  * print_numbers -  prints the numbers, from 0 to 9
- *
  * Return: void
  */
 
-void print_numbers(void)
+void print_numbers(void);
 {
 	char i;
 
 	for (i = '0'; i <= '9'; i++)
+	{
 		_putchar(i);
+	}
 	_putchar('\n');
 }

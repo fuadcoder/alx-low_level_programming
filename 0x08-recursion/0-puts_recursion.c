3e72204cfad6db5035cbf1f0fd212a8084a0e915
Horlams <olasupoolamide00@gmail.com>
2023-04-05 17:58:55 -0700

    0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _puts_recursion - function
+ * @s: input
+ * Return: 0
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+		_putchar('\n');
+}

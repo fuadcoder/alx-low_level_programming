cb0b41c0fba3ca43e99be5a20b89e0bb2f183ec4
Horlams <olasupoolamide00@gmail.com>
2023-04-11 18:38:30 -0700

    1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -11,14 +11,14 @@
 char *_strdup(char *str)
 {
 	char *strout;
-	unsigned int i, j;
+	int i, j;
 
 	if (str == NULL)
 		return (NULL);
 
 	for (i = 0; str[i] != '\0'; i++)
 
-	strout = (char *)malloc(sizeof(char) * (i + 1));
+	strout = malloc(sizeof(char) * (i + 1));
 
 	if (strout == NULL)
 		return (NULL);

b88dbb69023d8be2de815a1b770d5550f7813b0f
Horlams <olasupoolamide00@gmail.com>
2023-05-02 07:12:36 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,14 +9,14 @@
 
 size_t print_listint(const listint_t *h)
 {
-	size_t number = 0;
+	size_t num = 0;
 
 	while (h)
 	{
 		printf("%d\n", h->n);
-		number++;
+		num++;
 		h = h->next;
 	}
 
-	return (number);
+	return (num);
 }

43f3d851bd71454571aaf513d0774a26450c222d
Horlams <olasupoolamide00@gmail.com>
2023-05-08 17:42:04 -0700

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -16,7 +16,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	ssize_t w;
 	ssize_t r;
 
-	fd = open(filename, 0_RDONLY);
+	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
 	buf = malloc(sizeof(char) * letters);
@@ -24,6 +24,6 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	w = write(STDOUT_FILENO, buf, r);
 
 	free(buf);
-	clode(fd);
+	close(fd);
 	return (w);
 }
